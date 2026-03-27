74bb019c25957dbfc8ed1a5efe7e5f1e1a734399
Utkarsh Singh <utkarsh151008@gmail.com>
2026-03-27 22:07:06 +0530

    Add projectile motion simulation in Projectile.py
    This script simulates the projectile motion of an object based on user-defined velocity and angle, calculating horizontal and vertical distances over time.

---
+++ Projectile.py
@@ -0,0 +1,19 @@
+#This model simulates the distance travelled by the object and how up it has gone within due time
+import math 
+velocity = float(input("Enter Velocity in m/s: ")) #Enter the velocity in standardized unit
+angle = float(input("Enter the Angle: ")) #Enter the angle in Degrees
+theta = math.radians(angle) #Degrees is converted into Radians
+vx = velocity * math.cos(theta) 
+vy = velocity * math.sin(theta)
+print(f"{vx:.2f} m/s")
+print(f"{vy:.2f} m/s")
+g = 9.81  
+t = 0
+dt = 0.1 #Time Steps (Seconds)
+while True:
+    x = vx * t #Calculates Horziontal Distance
+    y = vy * t - 0.5 * g * t ** 2 #Calculates Vertical Distance
+    print(f"t={t:.2f}s  x={x:.2f}m  y={y:.2f}m")
+    if y < 0: 
+        break #Break the loop when object hits the ground
+    t += dt ## Moves time forward in small steps to approximate continuous motion
